deff68afeddd26ba1d30e5ab6090de39863875c1
Denisa Andra Ionitoiu <denisa.andra.ionitoiu@ibm.com>
2023-10-10 17:01:36 +0300

    Implemented update student feature.

--- src/main/java/com/springboot/StudentManagement/controller/StudentController.java
+++ src/main/java/com/springboot/StudentManagement/controller/StudentController.java
@@ -4,9 +4,7 @@ import com.springboot.StudentManagement.entity.Student;
 import com.springboot.StudentManagement.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -18,7 +16,7 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    //handler method to handle list students and return mode and view
+    // Handler method to handle list students and return mode and view
     @GetMapping("/students")
     public String listStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
@@ -36,6 +34,34 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
+        return "redirect:/students";
+
+    }
+    @GetMapping("/*")
+    public String redirectWelcomePageToStudentsView() {
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        return "edit_student";
+    }
+
+    @PostMapping("students/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+        // Get student from database by id
+        Student existingStudent = studentService.getStudentById(id);
+
+        // Update the existing student with the values trough the form
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        // Save updated student object
+        studentService.updateStudent(existingStudent);
+
         return "redirect:/students";
 
     }
